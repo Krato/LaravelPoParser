0d7708b589b3d2b2052616700464e1a3d336754c
Eric Lagarda <Krato@users.noreply.github.com>
2015-11-13 09:13:52 +0100

    Update ClassPoParser.php

--- src/ClassPoParser.php
+++ src/ClassPoParser.php
@@ -3,8 +3,6 @@
 use Exception;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
-use sepia\FileHandler;
-use sepia\PoParser;
 use Gettext\Extractors;
 use Gettext\Generators;
 use Gettext\Translations;
@@ -19,6 +17,7 @@ class ClassPoParser {
 
 	private static $config = [];
     private static $locale;
+    private static $currentTranslation;
 
 
     public function __construct()
@@ -89,27 +88,39 @@ class ClassPoParser {
         return $files;
     }
 
-    public static function getEntries($locale, $refresh = true)
-    {
-        if (empty($refresh) && ($cache = self::getCache($locale))) {
+    public static function getEntries($refresh = true){
+        if (empty($refresh) && ($cache = self::getCache(self::$locale))) {
             return $cache;
         }
-        $entries = clone self::scan();
-        $file = self::getFile($locale);
-        if (is_file(base_path()."/".$file.'mo')) {
-            $entries->mergeWith(Extractors\Mo::fromFile(base_path()."/".$file.'mo'));
+        $file = self::getFile(self::$locale);
+        if (is_file(base_path()."/".$file.'po')) {
+            $entries = Translations::fromMoFile(base_path()."/".$file.'mo');
         }
         return $entries;
     }
 
-
-    public static function getNewEntries($locale, $refresh = true)
+    public static function getAllEntries($refresh = true)
     {
-        if (empty($refresh) && ($cache = self::getCache($locale))) {
+        if (empty($refresh) && ($cache = self::getCache(self::$locale))) {
             return $cache;
         }
         $entries = clone self::scan();
-        $file = self::getFile($locale);
+        $file = self::getFile(self::$locale);
+        if (is_file(base_path()."/".$file.'mo')) {
+            $entries->mergeWith(Extractors\Mo::fromFile(base_path()."/".$file.'mo'));
+        }
+        self::$currentTranslation = $entries;
+        return $entries;
+    }
+
+
+    public static function getNewEntries($refresh = true)
+    {
+        if (empty($refresh) && ($cache = self::getCache(self::$locale))) {
+            return $cache;
+        }
+        $entries = clone self::scan();
+        $file = self::getFile(self::$locale);
         if (is_file(base_path()."/".$file.'mo')) {
             $entries->mergeWith(Extractors\Mo::fromFile(base_path()."/".$file.'mo'));
         }
@@ -119,33 +130,48 @@ class ClassPoParser {
                 $new_entries[] = $entry;
             }
         }
+        self::$currentTranslation = $new_entries;
         return $new_entries;
     }
 
-    private static function store($locale, $entries)
+
+    public static function UpdateAll($refresh = true){
+        if (empty($refresh) && ($cache = self::getCache(self::$locale))) {
+            return $cache;
+        }
+        $entries = clone self::scan();
+        $file = self::getFile(self::$locale);
+        if (is_file(base_path()."/".$file.'mo')) {
+            $entries->mergeWith(Extractors\Mo::fromFile(base_path()."/".$file.'mo'));
+            $entries->mergeWith(Extractors\Po::fromFile(base_path()."/".$file.'po'));
+        }
+
+        self::setTranslationObject($entries);
+        self::UpdateFiles();
+    }
+
+    private static function store($entries)
     {
 
-        $file = self::getFile($locale);
+        $file = self::getFile(self::$locale);
 
         $dir = base_path()."/".dirname($file);
 
         if (!is_dir($dir)) {
             mkdir($dir, 0755, true);
         }
-        $entries->setHeader('Language', $locale);
-        Generators\Mo::toFile($entries, base_path()."/".$file.'mo');
-        
+        $entries->setHeader('Language', self::$locale);
         Generators\Po::toFile($entries, base_path()."/".$file.'po');
         // Generators\PhpArray::toFile($entries, base_path()."/".$file.'php');
         return $entries;
     }
 
-    public static function setEntries($locale, $translations)
+    public static function setEntries($translations)
     {
         if (empty($translations)) {
             return true;
         }
-        $entries = self::getCache($locale) ?: (new Translations());
+        $entries = self::getCache(self::$locale) ?: (new Translations());
         foreach ($translations as $msgid => $msgstr) {
             $msgid = urldecode($msgid);
             if (!($entry = $entries->find(null, $msgid))) {
@@ -154,7 +180,7 @@ class ClassPoParser {
             $entry->setTranslation($msgid);
         }
 
-        self::store($locale, $entries);
+        self::store(self::$locale, $entries);
         return $entries;
     }
 
@@ -167,42 +193,56 @@ class ClassPoParser {
         # sudo locale-gen es_ES.UTF-8
         # sudo update-locale
 
-        $_ENV["LANG"] = $locale;
-        $_ENV["LANGUAGE"] = $locale;
+        // $_ENV["LANG"] = $locale;
+        // $_ENV["LANGUAGE"] = $locale;
 
-        $_ENV["LC_MESSAGES"] = $locale;
-        $_ENV["LC_PAPER"] = $locale;
-        $_ENV["LC_TIME"] = $locale;
-        $_ENV["LC_MONETARY"] = $locale;
-        setlocale(LC_MESSAGES, $locale);
-        setlocale(LC_COLLATE, $locale);
-        setlocale(LC_TIME, $locale);
-        setlocale(LC_MONETARY, $locale);
-        bindtextdomain(self::$config['domain'], self::$config['storage']);
-        bind_textdomain_codeset(self::$config['domain'], 'UTF-8');
-        textdomain(self::$config['domain']);
+        // $_ENV["LC_MESSAGES"] = $locale;
+        // $_ENV["LC_PAPER"] = $locale;
+        // $_ENV["LC_TIME"] = $locale;
+        // $_ENV["LC_MONETARY"] = $locale;
+        // setlocale(LC_MESSAGES, $locale);
+        // setlocale(LC_COLLATE, $locale);
+        // setlocale(LC_TIME, $locale);
+        // setlocale(LC_MONETARY, $locale);
+        // bindtextdomain(self::$config['domain'], self::$config['storage']);
+        // bind_textdomain_codeset(self::$config['domain'], 'UTF-8');
+        // textdomain(self::$config['domain']);
         # Also, we will work with gettext/gettext library
         # because PHP gones crazy when mo files are updated
         $path = base_path()."/".dirname(self::getFile(self::$locale));
         $file = $path.'/'.self::$config['domain'];
 
 
-        if (is_file($file.'.php')) {
-            $translations = $file.'.php';
-        } elseif (is_file($file.'.mo')) {
+
+        if(is_file($file.'.mo')) {
             $translations = Translations::fromMoFile($file.'.mo');
-        } elseif (is_file($file.'.po')) {
-            $translations = Translations::fromPoFile($file.'.po');
+        } 
+
+        if(is_file($file.'.po')) {
+            $translationsPo = Translations::fromPoFile($file.'.po');
+        }
+
+        if($translations){
+            $translations->mergeWith($translationsPo);
         } else {
+            $translations = $translationsPo;
+        }
+        
+        if(!$translations){
             $translations = new Translations();
         }
         
         $translations->setLanguage($locale);
         $translations->setHeader('LANGUAGE', $locale);
-        $trans = new Translator();
-        $trans->loadTranslations($translations);
 
-        Translator::initGettextFunctions($trans);
+
+        self::$currentTranslation = $translations;
+
+
+        // $trans = new Translator();
+        // $trans->loadTranslations($translations);
+
+        // Translator::initGettextFunctions($trans);
     }
 
     public static function updateMO(){
@@ -210,17 +250,68 @@ class ClassPoParser {
         $path = base_path()."/".dirname(self::getFile(self::$locale));
         $file = $path.'/'.self::$config['domain'];
 
-
-        // phpmo_convert( $file.'.po', [ 'output.mo' ] );
-
         $translations = Translations::fromPoFile($file.'.po');  
         $translations->toMoFile($file.'.mo');
+
+    }
+
+    public static function find($key){
+        $locale = self::$locale;
+       
+        $translation = self::$currentTranslation;
+        $found = $translation->find('', $key);
+         
+        return $found;
+    }
+
+    public static function setTranslation($translation, $string){
+
+        $translation->setTranslation($string);
+        return $translation;
     }
 
 
+    public function saveTranslation($key, $new_string, $plurals = false, $comments = false){
+
+        $translations = self::getTranslationObject();
+        
+        $translation = $translations->find(null, $key);
+
+        if ($translation) {
+            $translation->setTranslation($new_string);
+            if($plurals){
+                $translation->setPluralTranslation($plurals);
+            }
+            if($comments){
+                $translation->addComment($comments);
+            }
+            
+        }
+
+        self::UpdateFiles();
+        return true;
+       
+    }
+
+    public static function UpdateFiles(){
+        $translations = self::getTranslationObject();
+        $path = base_path()."/".dirname(self::getFile(self::$locale));
+        $file = $path.'/'.self::$config['domain'];
+        $translations->toPoFile($file.'.po');
+        self::updateMO();
+        clearstatcache();
+    }
+
+    public static function setTranslationObject($translation){
+        self::$currentTranslation = $translation;
+    }
+
+    public static function getTranslationObject(){
+        return self::$currentTranslation;
+    }
+
     public function FileHandler($file)
     {
-        
             return new \Sepia\FileHandler($file);
        
     } 
